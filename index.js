fee1aeedf9d6dcdd78833f637b1a35c253e76ded
Jose Seie <joseeugenioseie@hotmail.com>
2020-09-18 14:00:46 +0200

    Tratamento de excepções

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+let errorLogKey = 'EXP_LOG'
+
 function loginOrRegister (params) {	
 
-    return axios.post(paramObject.API_ENDPOINT, params.payload, params.header)
-        .then(response => {
-            return response
-        })
-        .catch(error => {
-            return error
-        })
+    try {
+
+        return axios.post(paramObject.API_ENDPOINT, params.payload, params.header)
+            .then(response => {
+                return response
+            })
+            .catch(error => {
+                return error
+            })
+
+    } catch(e) {
+        console.error (errorLogKey)
+    }
 
 }
 

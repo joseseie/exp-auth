476f15ce28fcf090edccdea1be08f33bdb1bad32
Jose Seie <joseeugenioseie@hotmail.com>
2020-09-21 12:49:06 +0200

    Método de reposição de senha integrado

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 let errorLogKey = 'EXP_LOG'
 
-function loginOrRegister (params) {	
+function handlePostRequest (params) {	
 
     try {
 
@@ -20,9 +20,65 @@ function loginOrRegister (params) {
 
     } catch(e) {
         console.error (errorLogKey + ': ' + e.message)
+        return e;
+    }
+
+}
+
+function loginOrRegister(params) {
+    return handlePostRequest(params);
+}
+
+function sendPasswordResetLink  (params) {
+    return handlePostRequest(params);
+}
+
+function composeUserToken (params) {	
+
+    try {
+
+        let responseData = params.data;
+        let token = responseData.token_type + ' ' + responseData.access_token;
+
+        return token;
+
+    } catch(e) {
+        console.error (errorLogKey + ': ' + e.message);
         return null;
     }
 
 }
 
-module.exports.loginOrRegister = loginOrRegister;
+function composeHeader (token, contentType = 'application/x-www-form-urlencoded') {
+    return {
+        headers: {
+            'Authorization': token,
+            'Content-Type': contentType,
+        }
+    };
+}
+
+function getUserInfoByToken (params) {
+
+    if(!(params && params.token)) {
+        console.error (errorLogKey + ': Dados do user não buscados, pois o \'Token\' não foi enviado como parametro');
+        return; // interrompe a execução
+    }
+
+    return handlePostRequest({
+        API_ENDPOINT: params.API_ENDPOINT,
+        header: composeHeader(params.token),
+        payload: {},
+    })
+    .then (resp => {
+        return resp.data
+    })
+    .catch (error => {
+        return error
+    })
+}
+
+module.exports.loginOrRegister          = loginOrRegister;
+module.exports.composeUserToken         = composeUserToken;
+module.exports.getUserInfoByToken       = getUserInfoByToken;
+module.exports.sendPasswordResetLink    = sendPasswordResetLink;
